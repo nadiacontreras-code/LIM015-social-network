5f9dc2ee6f2f0426c4fd4af9eb2d0bcb0eb5fc65
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-09-23 19:24:20 -0500

    Configuracion JSON y Firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
 
 import { myFunction } from './lib/index.js';
 
