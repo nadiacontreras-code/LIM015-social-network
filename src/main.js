85c0d301de425b4e15a52537e5e674b537e857e6
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-09-28 15:25:47 -0500

    Añadiendo seccion de registro

--- src/main.js
+++ src/main.js
@@ -1,46 +1,62 @@
 // Este es el punto de entrada de tu aplicacion
-// import { initializeApp } from 'firebase/app';
+//import { initializeApp } from 'firebase/app';
 
-// TODO: Replace the following with your app's Firebase project configuration
+//TODO: Replace the following with your app's Firebase project configuration
 /* const firebaseConfig = {
   //...
-};
-const app = initializeApp(firebaseConfig); */
+}; */
+
+//const app = initializeApp(firebaseConfig);
+
 
 /* import { myFunction } from './lib/index.js';
+
 myFunction(); */
 
+
 const rootSection = document.getElementById('root');
-const userContent = '<h1>User</h1>';
-const logOutContent = '<h1>logOut<h1>';
+
+const homeContent = `<h1>Home</h1>`;
+const aboutContent = `<section id="secformulario">
+                      <div class="div">
+                      <span>Registrate</span>
+                      <input id="name" type="text" placeholder="Nombre"><br><br>
+                      <input id="apellido" type="text" placeholder="Apellido"><br><br>
+                      <input id="newEmail" type="email" placeholder="Correo Electronico"><br><br> 
+                      <input id="newPassword" type="password" placeholder="Constraseña"><br><br> 
+                      <input id="newPassword2" type="password" placeholder="Repetir contraseña"><br><br> 
+                      <button id="btnRegistro" type="submit">Enviar</button>
+                      </div>
+                      </section>`;
 
 const linkContent = {
-  '#user': userContent,
-  '#logOut': logOutContent,
+  '#home': homeContent,
+  '#logout': aboutContent,
 };
 
 const routes = {
-  '/': userContent,
-  '/logOut': logOutContent,
+  '/': homeContent,
+  '/logout': aboutContent,
 };
 
-// Mostrar HTML correcto al recargar la pagina
+//Mostrar HTML correcto al recargar la pagina
 const pathname = window.location.pathname;
 rootSection.innerHTML = routes[pathname];
 
-// Cambiar ruta en la URL para que no use el '#', ej. '/about'
+//Cambiar ruta en la URL para que no use el '#', ej. '/about'
 const changeRoute = (hash) => {
-  // cambie la URL para no ocupar el '#'
-  if (hash === '#user') {
-    window.history.replaceState({}, 'user', '/');
-  } else if (hash === '#logout') {
+  //cambie la URL para no ocupar el '#'
+  if(hash === '#home'){
+    window.history.replaceState({}, 'home', '/');
+  }else if(hash === '#logout'){
     window.history.replaceState({}, 'logout', '/logout');
   }
-};
+}; 
 
 // Cambiar HTML al cliclea links (1):
 
-window.addEventListener('hashchange', () => {
+window.addEventListener('hashchange', () => { 
+  document.getElementById('secinicio').style.display = 'none';
   const hash = window.location.hash;
   rootSection.innerHTML = linkContent[hash];
   changeRoute(hash);
@@ -50,3 +66,10 @@ window.onpopstate = () => {
   const pathnames = window.location.pathname;
   rootSection.innerHTML = routes[pathnames];
 };
+
+
+const btnRegistro = document.querySelector('#btnRegistro');
+  btnRegistro.addEventListener('click', () => {
+    document.getElementById('secinicio').style.display = 'block';
+    
+});   
