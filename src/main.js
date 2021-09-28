2ccf0d2295f8e34fce798aca2b8e6efd7b04d71f
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-09-27 19:25:48 -0500

    Mostrar HTML correcto al mostrar la pagina

--- src/main.js
+++ src/main.js
@@ -15,3 +15,32 @@ myFunction(); */
 
 
 const rootSection = document.getElementById('root');
+
+const homeContent = `<h1>Home</h1>`;
+const aboutContent = `<h1>Acerca de</h1>`;
+
+const linkContent = {
+  '#home': homeContent,
+  '#about': aboutContent,
+};
+
+const routes = {
+  '/': homeContent,
+  '/about': aboutContent,
+};
+
+
+
+//Mostrar HTML correcto al recargar la pagina
+const pathname = window.location.pathname;
+rootSection.innerHTML = routes[pathname];
+
+//Cambiar ruta en la URL para que no use el '#', ej. '/about'
+const changeRoute = (hash) => {
+  //cambie la URL para no ocupar el '#'
+  if(hash === '#user'){
+    window.history.replaceState({}, 'user', '/');
+  }else if(hash === '#logout'){
+    window.history.replaceState({}, 'logout', '/logout');
+  }
+}; 
