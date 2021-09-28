7bd52476e33920290a606175670ee2ab435f5623
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-09-27 19:44:24 -0500

    REsolviendo problemas de slint

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
-//import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 /* const firebaseConfig = {
@@ -10,38 +10,35 @@ const app = initializeApp(firebaseConfig); */
 /* import { myFunction } from './lib/index.js';
 myFunction(); */
 
-
 const rootSection = document.getElementById('root');
 const userContent = '<h1>User</h1>';
 const logOutContent = '<h1>logOut<h1>';
 
 const linkContent = {
-  '#user': homeContent,
+  '#user': userContent,
   '#logOut': logOutContent,
-}
+};
 
 const routes = {
-  '/': homeContent,
-  '/logOut': logOutContent
-}
+  '/': userContent,
+  '/logOut': logOutContent,
+};
 
-
-
-//Mostrar HTML correcto al recargar la pagina
+// Mostrar HTML correcto al recargar la pagina
 const pathname = window.location.pathname;
 rootSection.innerHTML = routes[pathname];
 
-//Cambiar ruta en la URL para que no use el '#', ej. '/about'
+// Cambiar ruta en la URL para que no use el '#', ej. '/about'
 const changeRoute = (hash) => {
-  //cambie la URL para no ocupar el '#'
-  if(hash === '#user'){
+  // cambie la URL para no ocupar el '#'
+  if (hash === '#user') {
     window.history.replaceState({}, 'user', '/');
-  }else if(hash === '#logout'){
+  } else if (hash === '#logout') {
     window.history.replaceState({}, 'logout', '/logout');
   }
-}; 
+};
 
-//Cambiar HTML al cliclea links (1):
+// Cambiar HTML al cliclea links (1):
 
 window.addEventListener('hashchange', () => {
   const hash = window.location.hash;
@@ -50,6 +47,6 @@ window.addEventListener('hashchange', () => {
 });
 
 window.onpopstate = () => {
-  const pathname = window.location.pathname;
-  rootSection.innerHTML = routes[pathname];
+  const pathnames = window.location.pathname;
+  rootSection.innerHTML = routes[pathnames];
 };
