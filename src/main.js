2b38dd5304131c17f76b06c5c15c7dc0e7be01c4
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-09-30 10:26:04 -0500

    Creando function register

--- src/main.js
+++ src/main.js
@@ -11,14 +11,15 @@
 /* import { myFunction } from './lib/index.js';
 
 myFunction(); */
-const paginaInicio = document.getElementById('registroInicio');
+const paginaInicio = document.getElementById('secInicio');
 
 const rootSection = document.getElementById('root');
 
 const homeContent = '<h1>Home</h1>';
-const aboutContent = `<section id="secformulario">
+const logOutContent = '<h1>Cerrar Sesión</h1>';
+const registrationContent = `<section id="registroForm">
                       <div class="div">
-                      <span>Registrate</span>
+                      <span>Registrate</span><br>
                       <input id="name" type="text" placeholder="Nombre"><br><br>
                       <input id="apellido" type="text" placeholder="Apellido"><br><br>
                       <input id="newEmail" type="email" placeholder="Correo Electronico"><br><br> 
@@ -26,37 +27,52 @@ const aboutContent = `<section id="secformulario">
                       <input id="newPassword2" type="password" placeholder="Repetir contraseña"><br><br> 
                       <button id="btnRegistro" type="submit">Enviar</button>
                       </div>
-                      </section>`;
+                    </section>`;
 
 const linkContent = {
   '#home': homeContent,
-  '#logout': aboutContent,
+  '#logout': logOutContent,
+  '#registration': registrationContent,
 };
 
 const routes = {
   '/': homeContent,
-  '/logout': aboutContent,
+  '/logout': logOutContent,
+  '/registration': registrationContent,
 };
 
 // Mostrar HTML correcto al recargar la pagina
 const pathname = window.location.pathname;
 rootSection.innerHTML = routes[pathname];
 
-// Cambiar ruta en la URL para que no use el '#', ej. '/about'
+// Cambiar ruta en la URL para que no use el '#', ej. '/registration'
 const changeRoute = (hash) => {
   // cambie la URL para no ocupar el '#'
-  if (hash === '#home') {
+  /* if (hash === '#home') {
     window.history.replaceState({}, 'home', '/');
   } else if (hash === '#logout') {
     window.history.replaceState({}, 'logout', '/logout');
     paginaInicio.style.display = 'none';
+  } */
+  switch (hash) {
+    case '#home':
+      window.history.replaceState({}, 'home', '/');
+      paginaInicio.style.display = 'block';
+      break;
+    case '#logout':
+      window.history.replaceState({}, 'logout', '/logout');
+      break;
+    case '#registration':
+      window.history.replaceState({}, 'resgistration', '/registration');
+      break;
+    default:
+      break;
   }
 };
-
 // Cambiar HTML al cliclea links (1):
 
 window.addEventListener('hashchange', () => {
-  document.getElementById('secinicio').style.display = 'none';
+  // document.getElementById('secinicio').style.display = 'none';
   const hash = window.location.hash;
   rootSection.innerHTML = linkContent[hash];
   changeRoute(hash);
@@ -69,5 +85,6 @@ window.onpopstate = () => {
 
 const btnRegistro = document.querySelector('#btnRegistro');
 btnRegistro.addEventListener('click', () => {
-  document.getElementById('secinicio').style.display = 'block';
+  paginaInicio.style.display = 'none';
+  document.getElementById('registroForm').style.display = 'block';
 });
