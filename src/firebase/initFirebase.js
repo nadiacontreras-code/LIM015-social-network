3c23eee223a48aca5ced5d8bda87684ec7bfd01b
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-10-05 09:37:23 -0500

    Configuracion de firebase v.9.1.1(compatibilidad)

--- src/firebase/initFirebase.js
+++ src/firebase/initFirebase.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: 'AIzaSyBXTPdhvmRPgLp66a0-Z9fIQE2ylAWDwng',
   authDomain: 'getbbb-9b521.firebaseapp.com',
   projectId: 'getbbb-9b521',
@@ -6,4 +6,23 @@ const firebaseConfig = {
   messagingSenderId: '45913824265',
   appId: '1:45913824265:web:c0bdb3536c2081e6b1ecd8',
 };
+firebase.initializeApp(firebaseConfig); */
+
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXTPdhvmRPgLp66a0-Z9fIQE2ylAWDwng",
+  authDomain: "getbbb-9b521.firebaseapp.com",
+  projectId: "getbbb-9b521",
+  storageBucket: "getbbb-9b521.appspot.com",
+  messagingSenderId: "45913824265",
+  appId: "1:45913824265:web:c0bdb3536c2081e6b1ecd8"
+};
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
