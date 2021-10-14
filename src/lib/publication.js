8c14ee3c0e838a2ba82af955bfc1ea32d048b1e0
giovanna-crypto <giovannalopez826@gmail.com>
2021-10-13 19:18:02 -1100

    agregando grid y flex css al profile y publication

--- src/lib/publication.js
+++ src/lib/publication.js
@@ -3,7 +3,7 @@ export default () => {
   publicar.className = 'perfilSection';
   const prueba = `<article class="miPost"><br><textarea
    type="text" id="todo-input" placeholder="¿Qué deseas compartir?" autocomplete="off"/></textarea>
-   <button class="formButton">Compartir</button></article>`;
+   <button class="formButton id="btn-post">Compartir</button></article>`;
 
   publicar.innerHTML = prueba;
   return publicar;
