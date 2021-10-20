72da0a822c0309d6a08c9a8ce70d5ed2357a54fd
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-10-20 01:25:32 -0500

    funcion en firestore de deletePost al 100% y updatePost 50%

--- src/lib/cerrarSesion.js
+++ src/lib/cerrarSesion.js
@@ -1,1 +1,24 @@
-// import { signOut } from '../firebase/firebase-fn.js';
+// import { deletePost } from '../firebase/firestore.js';
+
+export const modaldeletePost = (section) => {
+  const newSectionDelete = document.createElement('section');
+  const modaldelete = document.createElement('section');
+  modaldelete.innerHTML += `
+        <p class="pDeleteMenu">¿Desea borrar el post?</p>
+        <ul class="deleteMenu">
+          <li id="idYes"><i class="fas fa-check"></i>Si</li>
+          <li id="idNo"><i class="fas fa-times"></i>No</li>
+        </ul>`;
+
+  /*  const idYes = newSectionDelete.querySelector('#idYes');
+  const idNo = newSectionDelete.querySelector('#idNo');
+  // eslint-disable-next-line no-undef
+  idYes.addEventListener('click', () => deletePost(doc.data().post)
+    .then((resp) => resp)
+    .catch((err) => console.error(err)));
+  idNo.addEventListener('click', () => {
+    section.classList.add('hide');
+  }); */
+  modaldelete.appendChild(newSectionDelete);
+  return section;
+};
--- src/lib/publication.js
+++ src/lib/publication.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 // eslint-disable-next-line import/named
+// import { modaldeletePost } from './cerrarSesion.js';
 import { getCurrentUser } from '../firebase/firebase-fn.js';
-import { getEachPostUser } from '../firebase/firestore.js';
+import { getEachPostUser, deletePost, updatePosts } from '../firebase/firestore.js';
 // import { createPost, getPost, getCurrentUser } from '../firebase/firebase-fn.js';
 
 export default () => {
@@ -63,11 +69,73 @@ export default () => {
           </section>
           <section id="likeToPost">
           <button type="button" id='${doc.id}' class="btnLike">Like </button>
+          <button type="button" id='${doc.id}' class="btnDelete">Delete </button>
+          <button type="button" id='${doc.id}' class="btnEdit" >Edit </button>
           <p type="text">0</p>
           </section>
         </section>
       </section>`;
 
+        const btnDelete = readPostSection.querySelector('.btnDelete');
+        // eliminar post
+        btnDelete.addEventListener('click', () => {
+          // eslint-disable-next-line no-alert
+          const confirmar = window.confirm('¿Estás seguro de que deseas borrar este post?');
+          if (confirmar) {
+            deletePost(doc.id);
+            console.log(deletePost(doc.id));
+          }
+        });
+
+        const btnEdit = readPostSection.querySelector('.btnEdit');
+
+        btnEdit.addEventListener('click', () => {
+          const idPost = doc.data().uid;
+          const publication = writeAndReadPost.querySelector('#contentTextPost');
+          // publication.readOnly = false;
+          const btnGuardar = writeAndReadPost.querySelector('#compartirPost');
+          btnGuardar.innerHTML = 'Guardar';
+          publication.value = doc.data().post;
+          writeAndReadPost.querySelector('#contentTextPost').innerHTML = publication;
+
+          btnGuardar.addEventListener('click', () => {
+            const nuevoText = writeAndReadPost.querySelector('#contentTextPost').value;
+            const actualizacionpost = updatePosts(idPost, nuevoText);
+            /* .then(() => {
+                publication.innerHTML = nuevoText;
+              }); */
+            return actualizacionpost;
+          });
+        });
+
+        // editar post
+        /*  function editar(id, newpost) {
+          // eslint-disable-next-line no-param-reassign
+          id = doc.data().uid;
+          newpost = writeAndReadPost.querySelector('#contentTextPost');
+          newpost.value = doc.data().post;
+          const boton = writeAndReadPost.querySelector('#compartirPost');
+
+          boton.addEventListener(() => {
+            const collectionPostEdit = firebase().firestore().collection('postPruebaNadia').doc(id);
+            const postedit = writeAndReadPost.querySelector('#contentTextPost').value;
+
+            return collectionPostEdit.update({
+              id: doc.id,
+              post: postedit,
+            }).then(() => {
+              console.log('Documento editado');
+              boton.innerHTML = 'Guardar';
+              writeAndReadPost.querySelector('#contentTextPost').value = '';
+            }).catch((error) => {
+              console.error('Error updating document: ', error);
+            });
+          });
+        }
+
+        btnEdit.addEventListener('click', () => {
+          editar();
+        }); */
         // console.log(publicPost);
         // publicPost.innerHTML += readPost;
         publicPost.appendChild(readPostSection);
@@ -109,37 +177,5 @@ export default () => {
     }
   });
 
-  /* getEachPostUser(uniqueId).then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data());
-      console.log(doc.data().post);
-      // getEachPostUser(uniqueId).onSnapshot((querySnapshot) => {
-    //querySnapshot.forEach((doc) => {
-      const readPost = `
-            <!-- SECCION DE  PUBLICACIONES / LO QUE SE POSTEO TODOS-->
-            <section id="wallAllPost">
-              <section class="eachPost">
-                <section id="userWhoPosted">
-                <p>${name == null ? email : name}</p>
-              </section>
-              <section id="userContentPosted">
-                <p id='${doc.id}'>${doc.data().post}</p>
-              </section>
-              <section id="likeToPost">
-              <button type="button" id='${doc.id}' class="btnLike">Like </button>
-              <p type="text">0</p>
-              </section>
-            </section>
-          </section>`;
-      const publicPost = writeAndReadPost.querySelector('.publicPost');
-      // console.log(publicPost);
-      publicPost.innerHTML += readPost;
-    });
-  })
-    .catch((error) => {
-      console.log('Error: ', error);
-    }); */
-  // });
   return writeAndReadPost;
 };
